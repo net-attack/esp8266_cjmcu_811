8ea5bb704554a0abcf94d53f59e103805d469016
Philipp Wagner <philwagner@me.com>
2022-07-08 16:38:48 +0200

    add src

--- esp8266_cjcmu8118/esp8266_cjcmu8118.ino
+++ esp8266_cjcmu8118/esp8266_cjcmu8118.ino
@@ -3,15 +3,62 @@
  * Setup Programm to test sensor 
  * or establish connection to influxdb
  */
-#define LOOPDELAY 100
+#include "Adafruit_CCS811.h"
+
+ 
+#define LOOPDELAY 200
+#define LED_PIN    2    
+
+Adafruit_CCS811 ccs;
+
 
 void setup() {
+  
   Serial.begin(9600);
-  Serial.println("Testing the connection to CJMCU8118")
+  //Serial.println("Testing the connection to CJMCU8118");  
+  pinMode(LED_BUILTIN, OUTPUT);
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+
+  //calibrate temperature sensor
+  while(!ccs.available()){
+    float temp = ccs.calculateTemperature();
+    //ccs.setTempOffset(-temp);
+  }
+  Serial.print("CO2[ppm/1000]");
+  Serial.print("\t");
+  Serial.print("TVOC[ppb]");
+  Serial.print("\t");
+  Serial.println("T[°C]");
 }
 
-int x = 0;
-void loop() {
-  Serial.println(x++);
+bool statusLED = false;
+
+void loop() {  
+  
+  statusLED = !statusLED;
+  digitalWrite(LED_BUILTIN, statusLED);   // Arduino: turn the LED on (HIGH)
+  /*
+  if(statusLED){
+    Serial.println("LOOP LED ON");    
+  }else{
+    Serial.println("LOOP LED OFF");  
+  }
+  */
+  if(ccs.available()){
+    float temp = ccs.calculateTemperature();
+    if(!ccs.readData()){
+      Serial.print(ccs.geteCO2()/1000);
+      Serial.print("\t");
+      Serial.print(ccs.getTVOC());
+      Serial.print("\t");
+      Serial.println(temp-70);
+    }else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
   delay(LOOPDELAY);
 }
